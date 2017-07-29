92448ff2a71fae9065f8f794f0be40e23289be42
mateuszzawadzki <mateuszzawadzki555@gmail.com>
2017-07-29 18:49:33 +0200

    rework

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -5,50 +5,50 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MergeSort {
-	public static Integer[] mergeSort(Integer[] array){
-		int n = array.length;
-		if(n == 1)
-			return array;
-		
-		
-		Integer[] splitted1 = Arrays.copyOfRange(array, 0, n/2);
-		Integer[] splitted2 = Arrays.copyOfRange(array, n/2, n);
-		
-		splitted1 = mergeSort(splitted1);
-		splitted2 = mergeSort(splitted2);
-		
-		return merge(splitted1, splitted2);
+
+	private static Integer[] auxiliary;
+
+	public static void mergeSort(Integer[] array){
+		auxiliary = new Integer[array.length];
+		sort(array, auxiliary, 0, array.length-1);
 	}
-	public static Integer[] merge(Integer[] array1, Integer[] array2){
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		Integer[] result = new Integer[array1.length + array2.length];
-		
-		while(i < array1.length && j < array2.length){
-			if(array1[i] > array2[j]){
-				result[k++] = array2[j++];
-			}
-			else{
-				result[k++] = array1[i++];
-			}
+
+	private static void sort(Integer[] array, Integer[] auxiliary, int lo, int hi){
+		if (hi <= lo) {
+			return;
 		}
-		
-		
-		while(i < array1.length){
-			result[k++] = array1[i++];
-		}
-		
-		while(j < array2.length){
-			result[k++] = array2[j++];
-		}
-		
-		return result;
+
+		int mid = lo + (hi - lo) / 2;
+		sort(array, auxiliary, lo, mid);
+		sort(array, auxiliary, mid+1, hi);
+		merge(array, auxiliary, lo, mid, hi);
 	}
+
+	private static void merge(Integer[] array, Integer[] auxiliary, int lo, int mid, int hi){
+		for (int k = lo; k <= hi; k++) {
+			auxiliary[k] = array[k];
+		}
+
+		int i = lo;
+		int j = mid + 1;
+
+		for (int k = lo; k<= hi; k++) {
+			if (i > mid ) {
+				array[k] = auxiliary[j++];
+			} else if (j > hi) {
+				array[k] = auxiliary[i++];
+			} else if (auxiliary[j] < auxiliary[i]) {
+				array[k] = auxiliary[j++];
+			} else if (auxiliary[j] > auxiliary[i]) {
+				array[k] = auxiliary[i++];
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		Integer[] array = new Integer[]{1,5,0,3,2,6,9,-1,7};
-		array = mergeSort(array);
-		List<Integer> list = new ArrayList<Integer>(Arrays.asList(array));
+		mergeSort(array);
+		List<Integer> list = new ArrayList<>(Arrays.asList(array));
 		list.forEach(System.out::print);
 	}
 }
--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -5,48 +5,59 @@ import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort {
-	public static Integer[] quickSort(Integer[] array){
-		return sort(array, 0, array.length-1);
+	public static void quickSort(Integer[] array) {
+		sort(array, 0, array.length-1);
 	}
-	static Integer[] sort(Integer[] array, int left, int right){
-				
-		if(array.length == 1 || right <= left)
-			return array;
-		
-		int i = left - 1;
-		int j = right + 1;
-		int pivot = array[(left + right) / 2];
-		
-		while(true){
-			
-			while(array[++i] < pivot)
-				;
-			
-			while(array[--j] > pivot)
-				;
-			
-			if(i <= j){
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-			else
-				break;
-		}
-		
-		if(j > left)
-			sort(array, left, j);
-		if(i < right)
-			sort(array, i, right);
-		
-		return array;
-	}
-	
 
+	private static void sort(Integer[] array, int lo, int hi) {
+		if (hi <= lo) {
+			return;
+		}
+		int j = partition(array, lo, hi);
+		sort(array, lo, j-1);
+		sort(array, j+1, hi);
+
+	}
+
+	private static int partition(Integer[] array, int lo, int hi) {
+		int p = lo;
+		int i = lo;
+		int j = hi + 1;
+
+		while (true) {
+			while (array[++i] < array[p]) {
+				if(i == hi) {
+					break;
+				}
+			}
+
+			while (array[--j] > array[p]) {
+				if(j == lo) {
+					break;
+				}
+			}
+
+			if (i >= j) {
+				break;
+			}
+
+			swap(array, i, j);
+		}
+
+		swap(array, j, p);
+
+		return j;
+	}
+
+	public static void swap(Integer[] array, int s1, int s2) {
+		int temp = array[s1];
+		array[s1] = array[s2];
+		array[s2] = temp;
+	}
 	
 	public static void main(String[] args) {
 		Integer[] array = new Integer[]{1,5,0,3,2,6,9,-1,7};
-		array = quickSort(array);
+		quickSort(array);
 		List<Integer> list = new ArrayList<Integer>(Arrays.asList(array));
 		list.forEach(System.out::print);
 	}
